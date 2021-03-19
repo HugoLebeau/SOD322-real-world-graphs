002672705fbe3271e08a7868a7475fce4ec94318
Hugo Lebeau <hug97@hotmail.fr>
2021-03-19 18:33:33 +0100

    graph plots

--- src/plots.py
+++ src/plots.py
@@ -1,3 +1,4 @@
+import networkx as nx
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -108,7 +109,11 @@ plt.show()
 # %% tme3-3 Rooted PageRank
 
 rootedpagerank = pd.read_csv("../outputs/rooted_page_rank_Magnus_Carlsen.csv")
+rootedpagerank = rootedpagerank.loc[page_num2name.index]
 rootedpagerank.sort_values(by="RootedPageRank", ascending=False, inplace=True)
+rootedpagerank["Name"] = page_num2name.loc[rootedpagerank["Node"]]
+
+print(rootedpagerank[:20])
 
 categ_num2name = pd.read_csv("../data/alr21--categNum2Name--enwiki-20071018.txt", sep=None, engine='python', header=None, index_col=0)
 categ_num2name.columns = ["Category"]
@@ -120,22 +125,41 @@ categ.index.name = 'Page'
 categ["Categories"] = categ["Categories"].apply(lambda x: x.split(' ') if x else [])
 categ["Categories"] = categ["Categories"].apply(lambda x: [int(c) for c in x])
 
-sorted_pagerank = pagerank.sort_values(by="0.15", ascending=False)
-id_chess = set(categ_num2name[categ_num2name["Category"].apply(lambda x : "chess" in x or "Chess" in x)].index)
+id_chess = set(categ_num2name[categ_num2name["Category"].apply(lambda x : "chess" in x.lower())].index)
 chess_pages = categ["Categories"].apply(lambda x: len(id_chess.intersection(x)) > 0)
 
-mask = rootedpagerank["RootedPageRank"] > 1e-5
+mask = rootedpagerank["RootedPageRank"] > 1e-7
 
 fig, ax = plt.subplots(2, 1, figsize=(6, 6))
 ax[0].plot(rootedpagerank[mask]["RootedPageRank"].values)
-ax[0].set_xlim(left=1)
 ax[0].grid(ls=':')
 ax[0].set_xscale('log')
 ax[0].set_yscale('log')
 ax[0].set_ylabel("Proximity to Magnus Carlsen")
-ax[1].plot(chess_pages.loc[sorted_pagerank["Node"]].cumsum().values)
+ax[1].plot(chess_pages.loc[rootedpagerank[mask]["Node"]].values.cumsum())
 ax[1].grid(ls=':')
 ax[1].set_xscale('log')
 ax[1].set_xlabel("Rank of the node")
 ax[1].set_ylabel("Number of nodes related to chess")
 plt.show()
+
+chess_boxing = pd.read_csv("../outputs/rooted_page_rank_Magnus_Carlsen_Mike_Tyson.csv")
+chess_boxing = chess_boxing.loc[page_num2name.index]
+chess_boxing.sort_values(by="RootedPageRank", ascending=False, inplace=True)
+chess_boxing["Name"] = page_num2name.loc[chess_boxing["Node"]]
+
+print(chess_boxing[:20])
+
+# %% tme4-1
+
+G = nx.read_edgelist("../outputs/gen_graph_0.5_0.01.csv")
+nx.draw(G, arrows=False, node_size=50)
+plt.title(r"$p = 0.5$   $q = 0.01$")
+
+G = nx.read_edgelist("../outputs/gen_graph_0.5_0.1.csv")
+nx.draw(G, arrows=False, node_size=50)
+plt.title(r"$p = 0.5$   $q = 0.1$")
+
+G = nx.read_edgelist("../outputs/gen_graph_0.9_0.01.csv")
+nx.draw(G, arrows=False, node_size=50)
+plt.title(r"$p = 0.9$   $q = 0.01$")
